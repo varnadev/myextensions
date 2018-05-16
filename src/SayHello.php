ddfa632ebcfdfe53ba5165f72bab8c1304bdd5d0
Varna <varna777@yandex.ru>
2018-05-16 16:24:19 +0300

    initial

---
+++ src/SayHello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: php1-16
+ * Date: 16.05.2018
+ * Time: 16:20
+ */
+namespace HelloWorld;
+class SayHello {
+    public static function world() {
+        return 'Hello World, Composer!';
+    }
+}
+?>
